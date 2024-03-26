faf758b24dd81a21da3ad82b57cb6dbf8dea6b8b
Clark Xu <yilinxu@brandeis.edu>
2024-03-26 18:51:39 -0400

    Update main.m

--- Network/matlab_version/main.m
+++ Network/matlab_version/main.m
@@ -1,5 +1,6 @@
 import param_to_array.*
 % def param
+% three space in array matches param for FS, RSA, and IB neuron
 C_m = [0.5, 1, 1] .* 1e-6;
 V_k = [-90, -90, -90] .* 1e-3;
 V_ca = [0, 0, 120] .* 1e-3;
@@ -21,12 +22,35 @@ N = N_FS + N_RSA + N_IB;
 
 V = zeros(1, N);
 
+% convert param array to length N
 C_m = param_to_array(C_m);
+V_k = param_to_array(V_k);
+V_na = param_to_array(V_na);
+V_l = param_to_array(V_l);
+V_t = param_to_array(V_t);
+g_k = param_to_array(g_k);
+g_m = param_to_array(g_m);
+g_ca = param_to_array(g_ca);
+g_na = param_to_array(g_na);
+g_l = param_to_array(g_l);
+tau_max = param_to_array(tau_max);
 
-% gating variables
+
+% initiate gating variables
 n = zeros(1, N);
 m = zeros(size(n));
 h = zeros(size(n));
 p = zeros(size(n));
 q = zeros(size(n));
 s = zeros(size(n));
+
+tmax = 10;          % total simulation time
+dt = 1e-4;          % 0.1ms time step
+t = 0: dt: tmax;    % time vector
+Nt = length(t);
+
+% define connectivity matrix
+E_el = zeros(N);    % connectivity of electrical synapses
+E_ch = zeros(N);    % connectivity of chemical synapses
+
+
