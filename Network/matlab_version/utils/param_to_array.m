8bd65c67f63bdc8f12c41595ce344d91bc5e54b4
Clark Xu <yilinxu@brandeis.edu>
2024-03-27 16:50:55 -0400

    add comment

--- Network/matlab_version/utils/param_to_array.m
+++ Network/matlab_version/utils/param_to_array.m
@@ -1,6 +1,7 @@
 function [param_array] = param_to_array(params)
+% convert param to 150 length array
     param_array = zeros(1, 150);
-    param_array(1:30) = params(1);
-    param_array(31:120) = params(2);
-    param_array(121:150) = params(3);
+    param_array(1:30) = params(1);      % FS
+    param_array(31:120) = params(2);    % RSA
+    param_array(121:150) = params(3);   % IB
 end
