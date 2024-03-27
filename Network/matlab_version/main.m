53d9310c7717a792c56a3ddae90e134e49355f06
Clark Xu <yilinxu@brandeis.edu>
2024-03-27 16:51:02 -0400

    Update main.m

--- Network/matlab_version/main.m
+++ Network/matlab_version/main.m
@@ -1,4 +1,7 @@
+addpath('utils')
 import param_to_array.*
+import gating_variable_update.*
+
 % def param
 % three space in array matches param for FS, RSA, and IB neuron
 C_m = [0.5, 1, 1] .* 1e-6;
@@ -19,8 +22,6 @@ N_FS = 30;
 N_RSA = 90;
 N_IB = 30;
 N = N_FS + N_RSA + N_IB;
-
-V = zeros(1, N);
 
 % convert param array to length N
 C_m = param_to_array(C_m);
@@ -48,9 +49,39 @@ tmax = 10;          % total simulation time
 dt = 1e-4;          % 0.1ms time step
 t = 0: dt: tmax;    % time vector
 Nt = length(t);
+V = zeros(Nt, N);
 
 % define connectivity matrix
-E_el = zeros(N);    % connectivity of electrical synapses
-E_ch = zeros(N);    % connectivity of chemical synapses
+E_el = rand(N)*.06e-3;    % connectivity of electrical synapses, 0-0.06mS
+E_ch = rand(N)*.1e-3;     % connectivity of chemical synapses, 0-0.1mS
+I_max = 1e-7;           % 0.1 uA maximum amplitude as input current
+
+for i = 2:Nt
+    I_inj = zeros(1, N);
+    
+    % update gating variable before each run
+    dmdt = gating_variable_update(V(i-1,:), m, 'm', V_t, tau_max);
+    dndt = gating_variable_update(V(i-1,:), n, 'n', V_t, tau_max);
+    dhdt = gating_variable_update(V(i-1,:), h, 'h', V_t, tau_max);
+    dpdt = gating_variable_update(V(i-1,:), p, 'p', V_t, tau_max);
+    dqdt = gating_variable_update(V(i-1,:), q, 'q', V_t, tau_max);
+    dsdt = gating_variable_update(V(i-1,:), s, 's', V_t, tau_max);
+
+    m = m + dt*dmdt;
+    n = n + dt*dndt;
+    h = h + dt*dhdt;
+    p = p + dt*dpdt;
+    q = q + dt*dqdt;
+    s = s + dt*dsdt;
+
+    
+    V(i, :) = 1./ C_m .* (I_inj - ...
+        g_k .* (n.^4).*(V(i-1,:)-V_k) -...
+        g_m .* p.*(V(i-1,:)-V_k) -...
+        g_ca .* (q.^2).*s.*(V(i-1,:)-V_ca) -...
+        g_na .* (m.^3).*h.*(V(i-1,:)-V_na) -...
+        g_l .* (V(i-1,:)-V_l));
+
+end
 
 
