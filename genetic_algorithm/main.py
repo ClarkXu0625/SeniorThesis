a484eb871c9f24273039356c6c443e241ce12f5c
Clark Xu <yilinxu@brandeis.edu>
2024-04-23 17:30:37 -0400

    ga  upload

---
+++ genetic_algorithm/main.py
@@ -0,0 +1,23 @@
+from ga import *
+
+on_start()
+
+on_fitness()
+on_parents()
+on_crossover()
+on_mutation()
+on_generation()
+
+on_fitness()
+on_parents()
+on_crossover()
+on_mutation()
+on_generation()
+
+on_fitness()
+on_parents()
+on_crossover()
+on_mutation()
+on_generation()
+
+on_stop()
