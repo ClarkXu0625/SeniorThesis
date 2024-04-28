9c60a88b1bf49ecd543bbbf89fb5c69ba123fbd4
Yilin Xu <yilinxu@brandeis.edu>
2024-04-28 15:01:26 -0400

    add tqdm package for prediction

--- train.py
+++ train.py
@@ -9,6 +9,7 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import train_test_split
+from tqdm import tqdm
 
 [neg_waveforms, pos_waveforms, neg_label, pos_label, fin_labels] = load_spike()
 [X, y] = transform(neg_waveforms, pos_waveforms, fin_labels)
@@ -21,12 +22,31 @@ X_train, X_val, y_train, y_val = \
     train_test_split(X_train, y_train, test_size=0.25, random_state=1)
 
 
-#Initialize and train model
+# #Initialize and train model
+# model = XCS(learning_iterations = 500)  # 5000
+# trainedModel = model.fit(X_train,y_train)
+
+# print(trainedModel)
+
+
+# trainedModel.predict(X_test)
+# trainedModel.score(X_test, y_test)
+
+
+# Initialize and train model
 model = XCS(learning_iterations = 500)  # 5000
 trainedModel = model.fit(X_train,y_train)
 
-print(trainedModel)
+print("Training completed.")
 
+print(model)
 
-trainedModel.predict(X_test)
-trainedModel.score(X_test, y_test)
+# Predict using the trained model
+predictions = []
+for i in tqdm(range(len(X_test)), desc="Prediction progress"):
+    prediction = model.predict(np.array([X_test[i]]))
+    predictions.append(prediction)
+
+# Calculate accuracy
+accuracy = model.score(X_test, y_test)
+print("Accuracy:", accuracy)
