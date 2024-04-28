5a81c67a68d80ab4bd4bff5ff9b923f15240a70f
Yilin Xu <yilinxu@brandeis.edu>
2024-04-28 14:37:13 -0400

    remove pca transform from main

--- classifier_neuRecommend/main.py
+++ classifier_neuRecommend/main.py
@@ -20,36 +20,13 @@ from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import FunctionTransformer
 from sklearn.model_selection import GridSearchCV
 import xgboost as xgb
+from pca_transform import transform
 
-
-# load file
-final_dataset = load_spike()
-[neg_waveforms, pos_waveforms, neg_label, pos_label, fin_labels] = final_dataset
-
-
-#####################################################
-## preparing dataset, PCA for dimension reduction ###
-#####################################################
-
-def zscore_custom(x):
-    return zscore(x, axis=-1)
-zscore_transform = FunctionTransformer(zscore_custom)
-
-# Aggregate all waveforms
-fin_data = np.concatenate([neg_waveforms, pos_waveforms])
-# Zscore to homogenize amplitudes
-zscore_fin_data = zscore_transform.transform(fin_data)
-# No need to fit PCA to ALL datapoints
-pca_obj = pca(n_components=10).fit(zscore_fin_data[::1000])
-print(f'Explained variance : {np.sum(pca_obj.explained_variance_ratio_)}')
-pca_data = pca_obj.transform(zscore_fin_data)
-
-# Scale PCA components
-scaler_obj = StandardScaler().fit(pca_data)
-X = scaler_obj.transform(pca_data)
-y = fin_labels
+[neg_waveforms, pos_waveforms, neg_label, pos_label, fin_labels] = load_spike()
+[X, y] = transform(neg_waveforms, pos_waveforms, fin_labels)
 
 print(X.shape)
+print(X)
 
 # plt.scatter(X[:,0], X[:,1], color='red', s=100, marker='o')  # `s` is size, `marker` is the style of marker
 # plt.grid(True)  # Optional: adds a grid to the background
