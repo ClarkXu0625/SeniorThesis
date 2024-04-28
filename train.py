fafb4b0a191d6d63ac76a0405ab71fd8c99cff52
Clark Xu <yilinxu@brandeis.edu>
2024-04-28 16:28:56 -0400

    delete package not needed

--- train.py
+++ train.py
@@ -1,12 +1,10 @@
 import numpy as np
-import pandas as pd
 from classifier_neuRecommend.pca_transform import transform
 from classifier_neuRecommend.load_spikes import load_spike
 
 from skXCS import XCS
 from skXCS import StringEnumerator
 import numpy as np
-import pandas as pd
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
