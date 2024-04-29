47cbc2776bcced428b58e402e1bb4b80fe34e0ff
Clark Xu <yilinxu@brandeis.edu>
2024-04-28 20:52:18 -0400

    Update train.py

--- train.py
+++ train.py
@@ -1,5 +1,5 @@
 import numpy as np
-from classifier_neuRecommend.pca_transform import transform
+from classifier_neuRecommend.transform import pca_transform
 from classifier_neuRecommend.load_spikes import load_spike
 
 from skXCS import XCS
@@ -11,29 +11,28 @@ from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_sc
 from tqdm import tqdm
 
 [neg_waveforms, pos_waveforms, neg_label, pos_label, fin_labels] = load_spike()
-[X, y] = transform(neg_waveforms, pos_waveforms, fin_labels)
+[X, y] = pca_transform(neg_waveforms, pos_waveforms, fin_labels)
+# X = np.concatenate([neg_waveforms, pos_waveforms])
+# y = fin_labels
 
 # Split into Train, Test, and Validation sets
 X_train, X_test, y_train, y_test = \
-    train_test_split(X, y, test_size=0.25, random_state=1)
+    train_test_split(X, y, test_size=0.1, random_state=1)
 
 # X_train, X_val, y_train, y_val = \
 #     train_test_split(X_train, y_train, test_size=0.25, random_state=1)
 
 
-# #Initialize and train model
-# model = XCS(learning_iterations = 500)  # 5000
-# trainedModel = model.fit(X_train,y_train)
 
-# print(trainedModel)
-
-
-# trainedModel.predict(X_test)
-# trainedModel.score(X_test, y_test)
-
-
-# Initialize and train model
-model = XCS(learning_iterations = 5000)  # 5000
+# Initialize and train model, setting hyperparameters to maximize the perfomance of classification
+model = XCS(
+    N=2000,                 # Increased population size
+    beta=0.2,               # Adjust learning rate
+    theta_GA=25,            # Adjust GA threshold
+    e_0=0.01,         # Lower minimum error threshold
+    theta_sub=50,           # Experience threshold
+    learning_iterations=5000
+)
 trainedModel = model.fit(X_train,y_train)
 
 print("Training completed.")
@@ -61,3 +60,7 @@ print("Recall:", recall)
 # Calculate F1 score
 f1 = f1_score(y_test, predictions)
 print("F1 Score:", f1)
+
+
+# print(f'Score on training set : {model.score(X_train, y_train)}')
+print(f'Score on test set : {model.score(X_test, y_test)}')
